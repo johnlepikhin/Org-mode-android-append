569c0dca869b24650146156e02948d92e2424485
Evgenii Lepikhin <e.lepikhin@corp.mail.ru>
2017-12-17 12:40:18 +0300

    Add state selector

--- app/src/main/java/io/github/johnlepikhin/orgmodequicknotes/AddNoteActivity.java
+++ app/src/main/java/io/github/johnlepikhin/orgmodequicknotes/AddNoteActivity.java
@@ -4,8 +4,10 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -31,12 +33,22 @@ public class AddNoteActivity extends Activity {
         if (textValue == null) {
             textValue = "";
         }
+        String stateValue = MainAppWidgetConfigureActivity.getString(this, widgetID, "state_draft");
 
         final EditText title = findViewById(R.id.noteTitle);
         final EditText text = findViewById(R.id.noteText);
+        final Spinner state = findViewById(R.id.noteState);
+
         Button buttonSave = findViewById(R.id.noteSave);
         title.setText(titleValue, TextView.BufferType.EDITABLE);
         text.setText(textValue, TextView.BufferType.EDITABLE);
+        if (stateValue != null) {
+            ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.note_states, android.R.layout.simple_spinner_item);
+            adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+            state.setAdapter(adapter);
+            int spinnerPosition = adapter.getPosition(stateValue);
+            state.setSelection(spinnerPosition);
+        }
 
         buttonSave.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -50,19 +62,27 @@ public class AddNoteActivity extends Activity {
                 }
 
                 try {
-                    String output = String.format("* %s\n\n%s\n", titleValue, textValue);
+                    String stateVlue = state.getSelectedItem().toString();
+                    if (stateVlue.equals("none")) {
+                        stateVlue = "";
+                    } else {
+                        stateVlue = stateVlue + " ";
+                    }
+                    String output = String.format("* %s%s\n\n%s\n", stateVlue, titleValue, textValue);
                     String file_path = MainAppWidgetConfigureActivity.getString(AddNoteActivity.this, widgetID, "file_path");
                     if (file_path == null) {
                         Toast t = Toast.makeText(getApplicationContext(), "Empty file name. Copy note contents and re-add the widget.", Toast.LENGTH_LONG);
                         t.show();
                         return;
                     }
+                    Log.d("SAVE", "Use file_path: " + file_path);
                     File file = new File(file_path);
                     FileOutputStream stream = new FileOutputStream(file, true);
                     stream.write(output.getBytes());
                     stream.close();
                     title.getText().clear();
                     text.getText().clear();
+                    state.setSelection(0);
                     finish();
                 } catch (Exception e) {
                     Toast t = Toast.makeText(getApplicationContext(), "Failed to save file", Toast.LENGTH_SHORT);
@@ -78,12 +98,11 @@ public class AddNoteActivity extends Activity {
 
         EditText title = findViewById(R.id.noteTitle);
         EditText text = findViewById(R.id.noteText);
+        Spinner state = findViewById(R.id.noteState);
 
-        String titleValue = title.getText().toString();
-        String textValue = text.getText().toString();
-
-        MainAppWidgetConfigureActivity.putString(this, widgetID, "title_draft", titleValue)
-                .putString("text_draft", textValue)
+        MainAppWidgetConfigureActivity.putString(this, widgetID, "title_draft", title.getText().toString())
+                .putString("text_draft", text.getText().toString())
+                .putString("state_draft", state.getSelectedItem().toString())
                 .apply();
     }
 }
